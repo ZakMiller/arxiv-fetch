9e230406d1b09f75061258ac680599e08818bb38
zak <zak@zakmiller.com>
2018-09-14 18:01:26 -0400

    download urls

--- arxiv-fetch.go
+++ arxiv-fetch.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"strings"
 	"fmt"
 	"strconv"
+
+	"net/http"
+	"io"
+	"os"
 
 	"github.com/devincarr/goarxiv"
 	"golang.org/x/tools/blog/atom"
@@ -13,7 +18,7 @@ type article struct {
 	url   string
 }
 
-func getArticles(term string, count int) []*article{
+func getArticles(term string, count int) []article {
 	s := goarxiv.New()
 	s.AddQuery("search_query", fmt.Sprintf("all:%v", term))
 	s.AddQuery("max_results", strconv.Itoa(count))
@@ -22,14 +27,12 @@ func getArticles(term string, count int) []*article{
 		fmt.Println(error)
 	}
 
-
-	articles := make([]*article, count)
+	articles := make([]article, count)
 	for _, entry := range result.Entry {
 		if article, ok := getArticle(entry); ok {
-			articles = append(articles, article)
+			articles = append(articles, *article)
 		}
 	}
-
 	return articles
 }
 
@@ -42,9 +45,35 @@ func getArticle(e *atom.Entry) (*article, bool) {
 	return nil, false
 }
 
-func main() {
-	articles := getArticles("google", 10)
-	for _, article := range articles {
-		fmt.Println(article)
+func downloadFile(filepath string, url string) error {
+
+	_ = os.Mkdir("articles", 0755)
+	// Create the file
+	out, err := os.Create("articles/" + filepath)
+	if err != nil {
+		return err
 	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	 articles := getArticles("google", 10)
+	 for _, article := range articles {
+		 downloadFile(fmt.Sprintf("%v.pdf", strings.TrimSpace(article.title)), article.url)
+	 }
 }
